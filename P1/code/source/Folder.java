2b576fab2a27d7e35770de2425f3f702c59cc925
Twisha Roy <twisha.roy@softwareag.com>
2014-03-05 16:52:43 +0530

    Commit P1

---
+++ P1/code/source/Folder.java
@@ -0,0 +1,40 @@
+
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2014-03-05 16:50:31 IST
+// -----( ON-HOST: VMSuiteCSI.eur.ad.sag
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class Folder
+
+{
+	// ---( internal utility methods )---
+
+	final static Folder _instance = new Folder();
+
+	static Folder _newInstance() { return new Folder(); }
+
+	static Folder _cast(Object o) { return (Folder)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void JS (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(JS)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
